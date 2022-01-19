69da4e73fbc29ddb859c1969810007bf80d3d10d
Saral Shrivastava <shrivastavaa278@gmail.com>
2022-01-19 20:26:58 +0530

    removed unused code

--- src/__test__/category.test.ts
+++
@@ -1,8 +0,0 @@
-import supertest from "supertest";
-describe("category", () => {
-  describe("get category route", () => {
-    it("should return true", () => {
-      expect(true).toBe(true);
-    });
-  });
-});
